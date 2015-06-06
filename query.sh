1d3d4d3f333c0321cff2d684ba71617b7cf5797e
Kristofer Lindberg <k.lindberg@gmail.com>
2015-06-06 22:59:36 +0200

    seach in other dirs as well

--- query.sh
+++ query.sh
@@ -4,25 +4,41 @@
 # of words
 # When we have matches open vim buffers for all of the matching files
 
+# Do we have a directory as last argument
+a=("$@")
+LAST=${a[-1]}
+if [ -d "$LAST" ]; then
+    pushd $LAST
+fi
+
+# recursive search, ignore case, only get one line per file
+# ignore binary
+FLAGS="-rilI"
+
 if [ "$1" = "and" ]; then
     shift
     MATCH="$1\|$2"
-    RES=`grep -ril "$1" * | xargs grep -li "$2"`
+    RES=`grep $FLAGS "$1" * | xargs grep -li "$2"`
 elif [ "$1" = "or" ]; then
     shift
     MATCH="$1\|$2"
-    RES=`grep -ril "$MATCH" *`
+    RES=`grep $FLAGS "$MATCH" *`
 elif [ "$1" = "match" ]; then
     shift
-    RES=`grep -rilw $1 *`
+    RES=`grep $FLAGS -w $1 *`
 else
-    RES=`grep -ril $1 *`
+    RES=`grep $FLAGS $1 *`
 fi
 
+# open vim with matching files
 if [ "$RES" = "" ]; then
     echo "Cannot find any matching files"
 elif [ -n "$MATCH" ]; then
     vim +/"$MATCH" $RES
 else
     vim +/$1 $RES
+fi
+
+if [ -d "$LAST" ]; then
+    popd
 fi
