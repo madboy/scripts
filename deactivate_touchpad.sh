dd35f6ff75906d36c311f9c79a28debb5ea6a5af
Kristofer Lindberg <k.lindberg@gmail.com>
2019-05-02 20:23:10 +0200

    minor tweaks

--- deactivate_touchpad.sh
+++ deactivate_touchpad.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-DEVICEID=`xinput | grep "Synaptics TouchPad" | awk '{ print $6 }' | tr '=' ' ' | awk '{ print $2 }'`
+DEVICEID=`xinput | grep "Synaptics" | awk '{ print $6 }' | tr '=' ' ' | awk '{ print $2 }'`
 STATE=`xinput list-props $DEVICEID | grep "Device Enabled" | awk '{ print $4 }'`
 
 if [ "$STATE" = "0" ]; then
